c8b37b0ed1a18726b6d5c540dc2694c74ccd9778
AriFlo <ariflo@arianflores.com>
2016-04-12 20:48:51 -0700

    finished two hash table excercises

--- hash_tables/hashTable.js
+++ hash_tables/hashTable.js
@@ -3,18 +3,55 @@
 
 function isUnique (str){
 
+	var dict = {};
+
 	for(var i = 0; i < str.length; i++){
-		for(var j = i; j < str.length; j++){
-			if(str.charCodeAt(i) === str.charCodeAt(j+1)){
-				return false;
-			}else{
-				return true;
-			}
+		if(!dict[str[i]]){
+			dict[str[i]] = str.charCodeAt(i);  
+		}else{
+			return false; 
+		}
+	}
+	return true;
+}
+
+//String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. 
+//Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring 
+//(e.g., "waterbottle" is a rotation of "erbottlewat").
+
+function isRotated (s1, s2) {
+
+	var lowS1 = s1.toLowerCase();
+	var lowS2 = s2.toLowerCase();
+
+	for(var i = 1; i < lowS1.length; i++){
+
+		var frontStr = lowS1.slice(0, i);
+		var backStr = lowS1.slice(i, lowS1.length);
+		backStr += frontStr;
+
+		if(backStr === lowS2){
+			return true;
+		}
+	}
+	return false;
+	
+	
+
+	function _isSubstring (string1, string2){
+		if(string1.indexOf(string2) > -1){
+			return true;
+		}else{
+			return false;
 		}
 	}
 }
 
 
+
+
 module.exports = {
-	isUnique:isUnique
+	isUnique:isUnique,
+	isRotated:isRotated
 };
+
--- hash_tables/test/Test.js
+++ hash_tables/test/Test.js
@@ -23,6 +23,24 @@ describe('hash tables', function(){
     	expect(hashTables.isUnique("436*^%$")).to.equal(true);
     	expect(hashTables.isUnique("()7&tom")).to.equal(true);
     	done();
+    })        
+
+    it("isSubstring should return true if a s2 is a rotation of s1", function(done){
+    	expect(hashTables.isRotated("waterbottle", "erbottlewat")).to.equal(true);
+    	expect(hashTables.isRotated("bananas", "asbanan")).to.equal(true);
+    	expect(hashTables.isRotated("cookies", "iescook")).to.equal(true);
+    	expect(hashTables.isRotated("blanket", "nketBla")).to.equal(true);
+    	expect(hashTables.isRotated("Magneto", "netomag")).to.equal(true);
+    	expect(hashTables.isRotated("%$$%^^", "%^^%$$")).to.equal(true);
+    	done();
+    })        
+
+    it("isSubstring should return false if a s2 is NOT a rotation of s1", function(done){
+    	expect(hashTables.isRotated("Joel", "pizza")).to.equal(false);
+    	expect(hashTables.isRotated("ellie", "elmo")).to.equal(false);
+    	expect(hashTables.isRotated("arizona", "anton")).to.equal(false);
+    	expect(hashTables.isRotated("^&&^", "^%^**")).to.equal(false);
+    	done();
     })    
 
 })
