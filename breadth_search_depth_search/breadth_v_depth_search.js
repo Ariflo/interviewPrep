6520a8886721fde0d99f3a5b8b8c8c50f6069fb7
AriFlo <ariflo@arianflores.com>
2016-04-15 13:52:36 -0700

    ran a few basics

--- breadth_search_depth_search/breadth_v_depth_search.js
+++ breadth_search_depth_search/breadth_v_depth_search.js
@@ -1,45 +1,93 @@
 //
 function breadthSearch (node, searchValue){
-	return ;
+	
 }
 
 //
 function depthSearch(node, searchValue){
-	return ;
+	
 }
 
 //return's the nth number of the fibonacci sequence 
 //ie...0, 1, 1, 2, 3, 5, 8, 13, 21, 34...
-// function fibonacci (n){
-// 	if (n === 0 || n === 1){
-// 		return 1; 
-// 	}
+function fibonacci (n){
+	if (n === 0 || n === 1){
+		return 1; 
+	}
 
-// 	var curNum = 0;
-// 	var nextNum = 1;
-// 	var fibNum;
+	var curNum = 0;
+	var nextNum = 1;
+	var fibNum;
 
-// 	for(var i = 1; i < n; i++){
-// 		fibNum = curNum + nextNum;
-// 		curNum = nextNum; 
-// 		nextNum  = fibNum;
-// 	}
-// 	return fibNum;
-// }
+	for(var i = 1; i < n; i++){
+		fibNum = curNum + nextNum;
+		curNum = nextNum; 
+		nextNum  = fibNum;
+	}
+	return fibNum;
+}
 
-// function factorial (n){
-// 	if(n === 0){
-// 		return 1;
-// 	}
+function factorial (n){
+	if(n === 0){
+		return 1;
+	}
 
-// 	var ft = factorial(n - 1) * n;
+	var ft = factorial(n - 1) * n;
 
-// 	return ft;
-// }
+	return ft;
+}
+
+//function will return true or false depending if the num is a prime number
+function isPrime (num){
+	//a prime number is only divisible by 1 and itself
+
+	// run a loop backwards from the number down to 1
+	//if it returns no remainder for any other number than one and itself 
+	//it is not a prime
+
+	//if any of the numbers in the seqence are multiples of num
+	//other than num itself and one
+	//return false
+	for (var i = num - 1 ; i > 1; i--) {
+
+		if(num % i === 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+//diffrence between the sum of the squares and the square of the sums of the first nth numbers
+function sOfSq(n){
+
+	return  _squareOfSum(n) - _sumOfSquares(n); 
+
+	function _sumOfSquares(num){
+		var sqTotal = 0;
+
+		for (var i = num; i > 0; i--) {
+			var sq = i * i; 	
+			sqTotal += sq;
+		}
+		return sqTotal;
+	}
+
+	function _squareOfSum(num) {
+		var sumTotal = 0;
+
+		for (var i = num; i > 0; i--) {
+			sumTotal += i; 
+		}
+
+		return sumTotal * sumTotal;
+	}
+}
 
 module.exports = {
 	breadthSearch: breadthSearch,
 	depthSearch:depthSearch,
-	// fibonacci: fibonacci,
-	// factorial: factorial
+	isPrime:isPrime,
+	sOfSq: sOfSq,
+	fibonacci: fibonacci,
+	factorial: factorial
 };
--- breadth_search_depth_search/test/Test.js
+++ breadth_search_depth_search/test/Test.js
@@ -25,8 +25,32 @@ describe('fibonacci', function(){
 describe('factorial', function(){
 
     it("factorial should return 120 with an input of 5", function(done){
-    	expect(bvds.factorial(5)).to.equal(120);
-    	done();
+     expect(bvds.factorial(5)).to.equal(120);
+     done();
     })         
+})
 
+describe('isPrime', function(){
+
+    it("should return true if the number is a prime number", function(done){
+            expect(bvds.isPrime(7)).to.equal(true);
+            expect(bvds.isPrime(5)).to.equal(true);
+            expect(bvds.isPrime(2)).to.equal(true);
+            done();
+    })       
+
+     it("should return false if the number is not a prime number", function(done){
+            expect(bvds.isPrime(4)).to.equal(false);
+            expect(bvds.isPrime(378)).to.equal(false);
+        expect(bvds.isPrime(6)).to.equal(false);
+        done();
+    })         
+})
+
+describe('sOfSq', function(){
+
+    it("should return true if the number is a prime number", function(done){
+            expect(bvds.sOfSq(10)).to.equal(2640);
+            done();
+    })           
 })
