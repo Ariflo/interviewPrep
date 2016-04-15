cc025f377a622de3e6d530b83803a2a5795027e8
AriFlo <ariflo@arianflores.com>
2016-04-14 19:40:37 -0700

    added binary search

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -1,9 +1,8 @@
 //Binary search will return the index of the search value 
-
 function binarySearch (arr, searchValue){
-	return false;
+	
 }
 
 module.exports = {
 	binarySearch: binarySearch
-}
+};
--- binary_search/test/Test.js
+++ binary_search/test/Test.js
@@ -1,12 +1,12 @@
 var request = require('supertest')
     , expect = require('chai').expect
     , session = require('supertest-session')
-    , bs = require ('../binary_search');
+    , bs = require('../binary_search');
 
 describe('binary search', function(){
 
     it("binarySearch should return the index of the search value", function(done){
-    	expect(bs.binarySearch(['a','b','c','d','e'], 'd').to.equal(3);
+    	expect(bs.binarySearch(['a','b','c','d','e'], 'd')).to.equal(3);
     	done();
     })       
 
