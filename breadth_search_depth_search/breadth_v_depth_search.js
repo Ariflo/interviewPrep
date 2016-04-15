49a8d70d1bb8ca8c55795f611f00d48ca5c3bf73
AriFlo <ariflo@arianflores.com>
2016-04-15 11:13:15 -0700

    working on bredthvDepth search

--- breadth_search_depth_search/breadth_v_depth_search.js
+++ breadth_search_depth_search/breadth_v_depth_search.js
@@ -1,14 +1,45 @@
 //
 function breadthSearch (node, searchValue){
-	return true;
+	return ;
 }
 
 //
 function depthSearch(node, searchValue){
-	return true;
+	return ;
 }
+
+//return's the nth number of the fibonacci sequence 
+//ie...0, 1, 1, 2, 3, 5, 8, 13, 21, 34...
+// function fibonacci (n){
+// 	if (n === 0 || n === 1){
+// 		return 1; 
+// 	}
+
+// 	var curNum = 0;
+// 	var nextNum = 1;
+// 	var fibNum;
+
+// 	for(var i = 1; i < n; i++){
+// 		fibNum = curNum + nextNum;
+// 		curNum = nextNum; 
+// 		nextNum  = fibNum;
+// 	}
+// 	return fibNum;
+// }
+
+// function factorial (n){
+// 	if(n === 0){
+// 		return 1;
+// 	}
+
+// 	var ft = factorial(n - 1) * n;
+
+// 	return ft;
+// }
 
 module.exports = {
 	breadthSearch: breadthSearch,
-	depthSearch:depthSearch
+	depthSearch:depthSearch,
+	// fibonacci: fibonacci,
+	// factorial: factorial
 };
--- breadth_search_depth_search/test/Test.js
+++ breadth_search_depth_search/test/Test.js
@@ -6,7 +6,26 @@ var request = require('supertest')
 describe('binary search', function(){
 
     it("breadthSearch of this node for this value should return true", function(done){
-    	expect(bvds.breadthSearch()).to.equal(true);
+        expect(bvds.breadthSearch()).to.equal(true);
+        done();
+    })         
+
+})
+
+describe('fibonacci', function(){
+
+    it("fibonacci should return 8 with an input of 6", function(done){
+        expect(bvds.fibonacci(6)).to.equal(8);
+            expect(bvds.fibonacci(3)).to.equal(2);
+        done();
+    })         
+
+})
+
+describe('factorial', function(){
+
+    it("factorial should return 120 with an input of 5", function(done){
+    	expect(bvds.factorial(5)).to.equal(120);
     	done();
     })         
 
