5a7407223d01448f23bc9de568a046e9154bb40e
AriFlo <ariflo@arianflores.com>
2016-04-14 20:19:50 -0700

    finished binary search

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -1,8 +1,55 @@
 //Binary search will return the index of the search value 
 function binarySearch (arr, searchValue){
-	
+	//create a pointer to the front, end, and center of the array
+	var midpoint = Math.floor(arr.length/2);
+	var firstIndex = 0;
+	var lastIndex = arr.length - 1; 
+
+	//run a loop through the array changing the pointers until the search value is found
+	while(searchValue !== arr[midpoint]){
+
+		if (searchValue > arr[midpoint]){
+			firstIndex = midpoint + 1; 
+			midpoint = Math.floor((firstIndex + lastIndex)/ 2);
+
+		}else if(searchValue < arr[midpoint]){
+			lastIndex = midpoint - 1; 
+			midpoint  = Math.floor((firstIndex +lastIndex)/2);
+		}else{
+			return -1; 
+		}
+	}
+	return midpoint;
+}
+
+//Binary search will return the index of the search value (recursively)
+function binaryRecursive(arr, searchValue, firstIndex, lastIndex){
+
+	if(firstIndex === undefined || lastIndex === undefined){
+		firstIndex = 0; 
+		lastIndex = arr.length - 1;
+	}
+
+	var midpoint = Math.floor((firstIndex + lastIndex)/2);
+
+	if(searchValue === arr[midpoint]){
+		return midpoint;
+
+	}else if(searchValue > arr[midpoint]){
+		firstIndex = midpoint + 1; 
+		return binaryRecursive(arr, searchValue, firstIndex, lastIndex);
+
+	}else if (searchValue < arr[midpoint]){
+		lastIndex = midpoint - 1; 
+		return binaryRecursive(arr, searchValue, firstIndex, lastIndex);
+
+	}else{
+		return -1; 
+	}
+
 }
 
 module.exports = {
-	binarySearch: binarySearch
+	binarySearch: binarySearch,
+	binaryRecursive:binaryRecursive
 };
--- binary_search/test/Test.js
+++ binary_search/test/Test.js
@@ -5,8 +5,33 @@ var request = require('supertest')
 
 describe('binary search', function(){
 
-    it("binarySearch should return the index of the search value", function(done){
+    it("binarySearch of array ['a','b','c','d','e'], 'd' should return the index of 3", function(done){
     	expect(bs.binarySearch(['a','b','c','d','e'], 'd')).to.equal(3);
+    	expect(bs.binaryRecursive(['a','b','c','d','e'], 'd')).to.equal(3);
+    	done();
+    })         
+    
+    it("binarySearch of array ['a','b','c','d','e'], 'a' should return the index of 0", function(done){
+    	expect(bs.binarySearch(['a','b','c','d','e'], 'a')).to.equal(0);
+    	expect(bs.binaryRecursive(['a','b','c','d','e'], 'a')).to.equal(0);
+    	done();
+    })         
+
+    it("binarySearch of array ['a','b','c','d','e'], '9' should return the index of -1", function(done){
+    	expect(bs.binarySearch(['a','b','c','d','e'], '9')).to.equal(-1);
+    	expect(bs.binaryRecursive(['a','b','c','d','e'], '9')).to.equal(-1);
+    	done();
+    })         
+
+    it("binarySearch of array ['a','b','c','d','e'], 'b' should return the index of 1", function(done){
+    	expect(bs.binarySearch(['a','b','c','d','e'], 'b')).to.equal(1);
+    	expect(bs.binaryRecursive(['a','b','c','d','e'], 'b')).to.equal(1);
+    	done();
+    })         
+
+    it("binarySearch of array ['a','b','c','d','e'], '%$^&' should return the index of -1", function(done){
+    	expect(bs.binarySearch(['a','b','c','d','e'], '&^%&&')).to.equal(-1);
+    	expect(bs.binaryRecursive(['a','b','c','d','e'], '&^%&&')).to.equal(-1);
     	done();
     })       
 
