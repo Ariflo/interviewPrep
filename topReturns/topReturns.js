9d2400e8784bb46064ac513b4d1a77f07fd69a9e
ariflo <arian@jurispect.com>
2017-09-18 12:53:55 -0700

    adjusts top returns

--- topReturns/topReturns.js
+++ topReturns/topReturns.js
@@ -5,10 +5,36 @@ function topReturns(arr) {
 	for(var i = 0; i<arr.length; i++){
 		var currentPrice = arr [i];
 		min = Math.min(currentPrice, min);
-		var tmpProfit = currentPrice -  min
+		var tmpProfit = currentPrice - min
 		maxProfit = Math.max(maxProfit,tmpProfit);
 	}
 	return maxProfit;
 }
 
-console.log(topReturns([13, 10, 25, 6, 90]));
+console.log(topReturns([3,2,1]));
+
+//Given an array of stock prices, find the index of the optimal time to buy and the optimal time to sell
+function bestBuySellTimes(arr) {
+	var min;
+	var maxProfit = topReturns(arr);
+	
+	var dict = {};
+	for (var i = 0; i<arr.length; i++) {
+		dict[arr[i]] = i;
+	}
+	var new_arr = [];
+	
+	for(var i = 0; i<arr.length; i++) {
+		var currentPrice = arr [i];
+		min = Math.min(currentPrice, min);
+		var tmpProfit = currentPrice - min
+		if (maxProfit === tmpProfit) {
+			new_arr.push(dict[min]);
+			new_arr.push(dict[currentPrice]);
+		}
+	}
+	return new_arr;
+}
+
+console.log(bestBuySellTimes([13, 10, 25, 6, 90]));
+//[3, 4]
